1194650706dc4f75faf32bef58c1f6b6d711bc42
Umberto Loria SAM <umberto.loria@gmail.com>
2020-10-31 13:42:14 +0100

    Introduzione Annuncio, AnnuncioWidget e BachecaView

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'model/Annuncio.dart';
 import 'view/BachecaView.dart';
 
 void main() {
+  var msgs = [
+    Annuncio(
+        'In sala riunioni ci sono le pizzette per San Raffaele!',
+        DateTime.parse('2020-10-26 09:42:27'),
+        'Raffaele Contabile',
+        Annuncio.INFO),
+    Annuncio('Conte ci ha chiuso statevi a casa fino a lunedì',
+        DateTime.parse('2020-04-15 21:23:14'), 'Gaetano Gallo', Annuncio.ALERT),
+    Annuncio('Quarantena per il reparto NoSQL',
+        DateTime.parse('2020-04-02 20:35:44'), 'Mario Russo', Annuncio.URGENT)
+  ];
+
   runApp(new MaterialApp(
     title: 'App',
-    home: BachecaView(),
+    home: BachecaView(msgs),
     routes: <String, WidgetBuilder>{
-      '/bacheca': (BuildContext context) => BachecaView(),
+      '/bacheca': (BuildContext context) => BachecaView(msgs),
       // '/login': (BuildContext context) => Login(),
     },
   ));
---
+++ lib/widget/AnnuncioWidget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../model/Annuncio.dart';
+
+class AnnuncioWidget extends StatelessWidget {
+  static const PRIORITY2COLOR = {
+    Annuncio.URGENT: Color(0xffFF8E8E),
+    Annuncio.ALERT: Color(0xffFFE38E),
+    Annuncio.INFO: Color(0xff8EB7FF)
+  };
+  static const SHORT_ITA_MONTHS = [
+    '',
+    'gen',
+    'feb',
+    'mar',
+    'apr',
+    'mag',
+    'giu',
+    'lug',
+    'ago',
+    'set',
+    'ott',
+    'nov',
+    'dic'
+  ];
+
+  final Annuncio annuncio;
+
+  AnnuncioWidget(this.annuncio);
+
+  static String FORMAT_DATETIME(DateTime datetime) {
+    String result = '';
+    result += datetime.day.toString();
+    result += ' ';
+    result += AnnuncioWidget.SHORT_ITA_MONTHS[datetime.month];
+    if (DateTime.now().year != datetime.year)
+      result += ' ' + datetime.year.toString();
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: Card(
+            color: PRIORITY2COLOR[this.annuncio.priority],
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                child: Column(children: <Widget>[
+                  SizedBox(
+                      width: double.infinity,
+                      child: Text(annuncio.text,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color(0xff444444), fontSize: 16))),
+                  SizedBox(height: 10),
+                  SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                          FORMAT_DATETIME(annuncio.datetime) +
+                              ', ' +
+                              annuncio.author,
+                          textAlign: TextAlign.right,
+                          style: TextStyle(color: Color(0xff444444))))
+                ]))));
+  }
+}
