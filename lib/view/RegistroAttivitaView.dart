ba8ca8497281f76d46e1abe9c94c1411a0559443
Salvatore Arienzo <s.arienzo@studenti.unisa.it>
2020-12-03 18:23:14 +0100

    Aggiunto registro delle attività

--- lib/view/RegistroAttivitaView.dart
+++ lib/view/RegistroAttivitaView.dart
@@ -1,35 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:proximapp/widget/AppBarMaker.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class RegistroAttivitaView extends StatelessWidget {
+class RegistroAttivitaView extends StatefulWidget {
+  RegistroAttivitaView({Key key}) : super(key: key);
+
+
+  @override
+  _RegistroAttivitaView createState() => _RegistroAttivitaView();
+}
+
+class _RegistroAttivitaView extends State<RegistroAttivitaView> with TickerProviderStateMixin {
+  Map<DateTime, List> _events;
+  List _selectedEvents;
+  AnimationController _animationController;
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+    final _selectedDay = DateTime.now();
+    int oraENtrata = 9;
+    int oraUscita = 18;
+    String nota = "Dormivo sul lavoro";
+
+    _events = {
+      _selectedDay.add(Duration(days: 0)): ['Ingresso Ore: $oraENtrata', 'Uscita Ore: $oraUscita', nota,],
+      _selectedDay.add(Duration(days: -1)): ['Ingresso Ore: ', 'Uscita Ore:', ],
+      _selectedDay.add(Duration(days: -2)): ['Ingresso Ore: ', 'Uscita Ore:', ],
+      _selectedDay.add(Duration(days: -3)): ['Note: permesso visita medica',],
+
+    };
+
+
+    _selectedEvents = _events[_selectedDay] ?? [];
+    _calendarController = CalendarController();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _animationController.forward();
+}
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime day, List events, List holidays) {
+    print('CALLBACK: _onDaySelected');
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onVisibleDaysChanged');
+  }
+
+  void _onCalendarCreated(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onCalendarCreated');
+  }
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Registro Attività';
+    return Scaffold(
+      appBar: makeAppBar('Registra Accesso'),
+      backgroundColor: Color(0xfff5f5f5),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          _buildTableCalendar(),
+          // _buildTableCalendarWithBuilders(),
+          const SizedBox(height: 7.0),
+          _buildButtons(),
+          const SizedBox(height: 7.0),
+          Expanded(child: _buildEventList()),
+        ],
+      ),
+    );
+  }
 
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.hail),
-              title: Text('Giorno X'),
-            ),
-            ListTile(
-              leading: Icon(Icons.today_rounded),
-              title: Text('Orario di Accesso: '),
-            ),
-            ListTile(
-              leading: Icon(Icons.night_shelter_rounded),
-              title: Text('Orario di Uscita:'),
-            ),
-          ],
-
+  // Simple TableCalendar configuration (using Styles)
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      locale: "it_ITA",
+      calendarController: _calendarController,
+      events: _events,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        selectedColor: Colors.lightBlue,
+        todayColor: Colors.blueAccent,
+        markersColor: Colors.blueGrey,
+        outsideDaysVisible: false,
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonTextStyle:
+        TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.lightBlue,
+          borderRadius: BorderRadius.circular(16.0),
         ),
       ),
+      onDaySelected: _onDaySelected,
+      onVisibleDaysChanged: _onVisibleDaysChanged,
+      onCalendarCreated: _onCalendarCreated,
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: _calendarController.isSelected(date)
+            ? Colors.brown[500]
+            : _calendarController.isToday(date)
+            ? Colors.brown[300]
+            : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHolidaysMarker() {
+    return Icon(
+      Icons.add_box,
+      size: 20.0,
+      color: Colors.blueGrey[800],
+    );
+  }
+
+  Widget _buildButtons() {
+    final dateTime = _events.keys.elementAt(_events.length - 2);
+
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Mese'),
+              onPressed: () {
+                setState(() {
+                  _calendarController.setCalendarFormat(CalendarFormat.month);
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text('Settimana'),
+              onPressed: () {
+                setState(() {
+                  _calendarController.setCalendarFormat(CalendarFormat.week);
+                });
+              },
+            ),
+          ],
+        ),
+        const SizedBox(height: 15.0),
+      ],
+    );
+  }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.8),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        margin:
+        const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: ListTile(
+          title: Text(event.toString()),
+          onTap: () => print('$event tapped!'),
+        ),
+      ))
+          .toList(),
     );
   }
 }
